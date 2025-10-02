5207427829100aef42149f064969d0384dd165a0
Suzanne Pons <suzanne.pons68@gmail.com>
2025-10-02 14:14:24 +0200

    Exercice 5 ajout des ingredients et leurs quantité dans le tableau recipe_ingredients

--- exercice_recipe.sql
+++ exercice_recipe.sql
@@ -30,3 +30,16 @@ les instructions de votre choix. Pensez à alimenter votre base de données en c
 pouvoir lister les détails de cette recettes (ingrédients) */
 INSERT INTO recipe
 VALUES ("11", "pate_carbonara", "20", "Battre les jaunes d'œufs en y ajoutant 1 pincée de sel, 2 pincées de poivre et 40 g de parmigiano reggiano râpé. Remplir une grande casserole d'eau et la faire chauffer avec deux pincées de gros sel. Pendant ce temps, couper la pancetta en lamelles grossières et les faire dorer dans une poêle avec 1 cuillère à soupe d'huile d'olive. Quand l’eau dans la casserole commence à bouillir mettez vos pâtes. Prendre deux cuillères à soupe d'eau de cuisson des pâtes, les verser dans le jaunes d'œufs puis remuer. Réservez la pancetta et laissez la poêle au chaud. Une fois la cuisson des pâtes al dente, les égoutter sommairement et les mettre dans la poêle et remuer. Verser le mélange dans un saladier et incorporez la préparation des jaunes dœufs. Ajouter la pancetta et deux pincées de poivre. Servez.", "2")
+/* Ajout des ingredients et quantité */
+INSERT INTO recipe_ingredients (quantity, id_ingredient, id_recipe)
+VALUES
+(15, 20, 11),
+(6, 6, 11),
+(3, 5, 11),
+(6, 8, 11),
+(500, 39, 11),
+(50, 31, 11),
+(300, 40, 11);
+
+/* Modifier le nom de la recette ayant comme identifiant id_recette = 3 (nom de la recette à votre 
+convenance) */
