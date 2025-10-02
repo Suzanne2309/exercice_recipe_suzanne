40478c729661a89966a1dcaab35c319cc8b72bfa
Suzanne Pons <suzanne.pons68@gmail.com>
2025-10-02 13:37:03 +0200

    Correction exercice 2 et amélioration exo 3 et 4

--- exercice_recipe.sql
+++ exercice_recipe.sql
@@ -1,26 +1,30 @@
 /* Afficher toutes les recettes disponibles (nom de la recette, catégorie et temps de préparation) triées 
 de façon décroissante sur la durée de réalisation */
-SELECT recipe_name AS rn, preparation_time AS pt, category_name
-FROM recipe
-INNER JOIN category ON recipe.id_category = category.id_category
-ORDER BY pt DESC
+SELECT r.recipe_name, r.preparation_time, r.instructions, c.category_name
+FROM recipe r
+INNER JOIN category c ON r.id_category = c.id_category
 
 /* En modifiant la requête précédente, faites apparaître le nombre d’ingrédients nécessaire par recette.
  */
-SELECT id_recipe, Count(id_ingredient)
-FROM recipe_ingredients
-GROUP BY id_recipe
+SELECT r.recipe_name, r.preparation_time, r.instructions, c.category_name, COUNT(ri.id_ingredient) AS nbIngredients
+FROM category c
+INNER JOIN recipe r ON r.id_category = c.id_category
+INNER JOIN recipe_ingredients ri ON r.id_recipe = ri.id_recipe
+GROUP BY r.recipe_name, r.preparation_time, r.instructions, c.category_name
+ORDER BY r.preparation_time DESC
 
 /* Afficher les recettes qui nécessitent au moins 30 min de préparation
  */
-SELECT *
-FROM recipe
+SELECT r.recipe_name, r.preparation_time, r.instructions, c.category_name
+FROM recipe r
+INNER JOIN category c ON r.id_category = c.id_category
 WHERE preparation_time >= 30
 
 /* Afficher les recettes dont le nom contient le mot « Salade » (peu importe où est situé le mot en 
 question) */
-SELECT *
-FROM recipe
+SELECT r.recipe_name, r.preparation_time, r.instructions, c.category_name
+FROM recipe r
+INNER JOIN category c ON r.id_category = c.id_category
 WHERE recipe_name LIKE '%salade%'
 
 /* Insérer une nouvelle recette : « Pâtes à la carbonara » dont la durée de réalisation est de 20 min avec 
