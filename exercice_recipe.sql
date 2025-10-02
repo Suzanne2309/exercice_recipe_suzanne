799f73068d73c1ad35102cc8e9c00bb1c22f6cf0
Suzanne Pons <suzanne.pons68@gmail.com>
2025-10-02 13:39:22 +0200

    Ajustement exo 3 et 4

--- exercice_recipe.sql
+++ exercice_recipe.sql
@@ -15,16 +15,14 @@ ORDER BY r.preparation_time DESC
 
 /* Afficher les recettes qui nécessitent au moins 30 min de préparation
  */
-SELECT r.recipe_name, r.preparation_time, r.instructions, c.category_name
+SELECT r.recipe_name, r.preparation_time
 FROM recipe r
-INNER JOIN category c ON r.id_category = c.id_category
 WHERE preparation_time >= 30
 
 /* Afficher les recettes dont le nom contient le mot « Salade » (peu importe où est situé le mot en 
 question) */
-SELECT r.recipe_name, r.preparation_time, r.instructions, c.category_name
+SELECT r.recipe_name
 FROM recipe r
-INNER JOIN category c ON r.id_category = c.id_category
 WHERE recipe_name LIKE '%salade%'
 
 /* Insérer une nouvelle recette : « Pâtes à la carbonara » dont la durée de réalisation est de 20 min avec 
