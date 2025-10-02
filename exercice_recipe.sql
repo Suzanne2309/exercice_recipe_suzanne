0f4278e1c6edb4c598636e9ae4582907484a9408
Suzanne Pons <suzanne.pons68@gmail.com>
2025-10-02 13:59:33 +0200

    exercice 5 insertion d'une recette dans le tableau recipe

--- exercice_recipe.sql
+++ exercice_recipe.sql
@@ -28,3 +28,5 @@ WHERE recipe_name LIKE '%salade%'
 /* Insérer une nouvelle recette : « Pâtes à la carbonara » dont la durée de réalisation est de 20 min avec 
 les instructions de votre choix. Pensez à alimenter votre base de données en conséquence afin de 
 pouvoir lister les détails de cette recettes (ingrédients) */
+INSERT INTO recipe
+VALUES ("11", "pate_carbonara", "20", "Battre les jaunes d'œufs en y ajoutant 1 pincée de sel, 2 pincées de poivre et 40 g de parmigiano reggiano râpé. Remplir une grande casserole d'eau et la faire chauffer avec deux pincées de gros sel. Pendant ce temps, couper la pancetta en lamelles grossières et les faire dorer dans une poêle avec 1 cuillère à soupe d'huile d'olive. Quand l’eau dans la casserole commence à bouillir mettez vos pâtes. Prendre deux cuillères à soupe d'eau de cuisson des pâtes, les verser dans le jaunes d'œufs puis remuer. Réservez la pancetta et laissez la poêle au chaud. Une fois la cuisson des pâtes al dente, les égoutter sommairement et les mettre dans la poêle et remuer. Verser le mélange dans un saladier et incorporez la préparation des jaunes dœufs. Ajouter la pancetta et deux pincées de poivre. Servez.", "2")
